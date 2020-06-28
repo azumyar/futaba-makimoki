153a484ecfc6d80ae03fc424426633bf62511969
azumyar <yarukizero.dev@gmail.com>
2020-06-29 01:23:16 +0900

    period.01

--- MakiMoki/MakiMoki.Wpf/Properties/AssemblyInfo.cs
+++ MakiMoki/MakiMoki.Wpf/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("ふたば@MakiMoki(仮)")]
+[assembly: AssemblyTitle("ふたマキ")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ふたば@MakiMoki(仮)")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("ふたマキ")]
+[assembly: AssemblyCopyright("Copyright © by https://dev.yarukizero.net/")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
