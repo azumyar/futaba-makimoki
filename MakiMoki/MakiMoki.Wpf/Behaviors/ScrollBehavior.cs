a869e4e6dc38310e4f2e5319938d7f4b9cacd547
azumyar <yarukizero.dev@gmail.com>
2020-07-02 02:03:18 +0900

    スレの一番上と一番下に行くボタン追加

---
+++ MakiMoki/MakiMoki.Wpf/Behaviors/ScrollBehavior.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Yarukizero.Net.MakiMoki.Wpf.Behaviors {
+	class ScrollBehavior : Behavior<Control> {
+		private ScrollViewer scrollViewer;
+
+		protected override void OnAttached() {
+			base.OnAttached();
+			if(this.AssociatedObject is ScrollViewer o) {
+				this.scrollViewer = o;
+			} else {
+				this.AssociatedObject.Loaded += OnLoadedObject;
+			}
+		}
+
+		protected override void OnDetaching() {
+			base.OnDetaching();
+
+			if(this.scrollViewer != null) {
+				this.scrollViewer = null;
+			}
+			this.AssociatedObject.Loaded -= OnLoadedObject;
+		}
+
+		private void OnLoadedObject(object sender, RoutedEventArgs e) {
+			if(e.Source is DependencyObject o) {
+				this.scrollViewer = WpfUtil.WpfHelper.FindFirstChild<ScrollViewer>(o);
+			}
+		}
+
+		public void ScrollToTop() => this.scrollViewer?.ScrollToTop();
+		public void ScrollToBottom() => this.scrollViewer?.ScrollToBottom();
+	}
+}
