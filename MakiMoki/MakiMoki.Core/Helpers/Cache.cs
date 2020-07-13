887f95aa01de33f79f15541cf346258d9a56a2ca
azumyar <yarukizero.dev@gmail.com>
2020-07-13 21:34:54 +0900

    誤字修正

--- MakiMoki/MakiMoki.Core/Helpers/Cache.cs
+++ MakiMoki/MakiMoki.Core/Helpers/Cache.cs
@@ -5,27 +5,27 @@ using System.Text;
 
 namespace Yarukizero.Net.MakiMoki.Helpers {
 	public class WeakCache<TKey, TValue> where TValue:class {
-		private Dictionary<TKey, WeakReference<TValue>> cach = new Dictionary<TKey, WeakReference<TValue>>();
+		private Dictionary<TKey, WeakReference<TValue>> cache = new Dictionary<TKey, WeakReference<TValue>>();
 
 		public void Add(TKey key, TValue val) {
-			if(cach.ContainsKey(key)) {
-				this.cach[key] = new WeakReference<TValue>(val);
+			if(cache.ContainsKey(key)) {
+				this.cache[key] = new WeakReference<TValue>(val);
 			} else {
-				this.cach.Add(key, new WeakReference<TValue>(val));
+				this.cache.Add(key, new WeakReference<TValue>(val));
 			}
 		}
 
 		public bool TryGetTarget(TKey key, out TValue val) {
 			var r = false;
 			val = default;
-			if(this.cach.TryGetValue(key, out var c)) {
+			if(this.cache.TryGetValue(key, out var c)) {
 				if(c.TryGetTarget(out var v)) {
 					val = v;
 					r = true;
 				}
 			}
 
-			this.cach = this.cach
+			this.cache = this.cache
 				.Where(x => x.Value.TryGetTarget(out var _))
 				.ToDictionary(x => x.Key, y => y.Value);
 			return r;
@@ -33,7 +33,7 @@ namespace Yarukizero.Net.MakiMoki.Helpers {
 	}
 
 	public class TimerCache<TKey, TValue> {
-		private Dictionary<TKey, (DateTime Time, TValue Value)> cach = new Dictionary<TKey, (DateTime Time, TValue Value)>();
+		private Dictionary<TKey, (DateTime Time, TValue Value)> cache = new Dictionary<TKey, (DateTime Time, TValue Value)>();
 		private int spanSec;
 
 		public TimerCache() : this(3 * 3600) { } // デフォルト3時間は保持する
@@ -44,24 +44,24 @@ namespace Yarukizero.Net.MakiMoki.Helpers {
 		}
 
 		public void Add(TKey key, TValue val) {
-			if(cach.ContainsKey(key)) {
-				this.cach[key] = (DateTime.Now, val);
+			if(cache.ContainsKey(key)) {
+				this.cache[key] = (DateTime.Now, val);
 			} else {
-				this.cach.Add(key, (DateTime.Now, val));
+				this.cache.Add(key, (DateTime.Now, val));
 			}
 		}
 
 		public bool TryGetTarget(TKey key, out TValue val) {
 			var r = false;
 			val = default;
-			if(this.cach.TryGetValue(key, out var v)) {
+			if(this.cache.TryGetValue(key, out var v)) {
 				val = v.Value;
 				Add(key, val); // 登録時間を更新する
 				r = true;
 			}
 
 			var now = DateTime.Now.AddSeconds(-this.spanSec);
-			this.cach = this.cach
+			this.cache = this.cache
 				.Where(x => now <= x.Value.Time)
 				.ToDictionary(x => x.Key, y => y.Value);
 			return r;
