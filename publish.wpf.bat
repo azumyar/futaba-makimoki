e9c2ba076f191f60b84c76cbec6f6b4667b8e4eb
azumyar <yarukizero.dev@gmail.com>
2021-01-07 19:42:51 +0900

    バッチファイルを修正

--- publish.wpf.bat
+++ publish.wpf.bat
@@ -17,6 +17,7 @@ set CONF_FILE=publish.wpf.conf.json
 for /f %%a in ('powershell -Command "(Get-Content %CONF_FILE% | ConvertFrom-Json).version"') do set VERSION=%%a
 
 if "%1" == "beta" for /f %%a in ('powershell -Command "Write-Output('futamaki-{0:000}b{1:00}.zip' -f ((New-Object System.Version('%VERSION%')).Minor + 1), (New-Object System.Version('%VERSION%')).Build)"') do set OUTPUT_ZIP=%%a
+if "%1" == "" set OUTPUT_ZIP=
 if "%OUTPUT_ZIP%" == "" for /f %%a in ('powershell -Command "Write-Output('futamaki-{0:000}.zip' -f (New-Object System.Version('%VERSION%')).Minor)"') do set OUTPUT_ZIP=%%a
 
 @rem 事前チェック
@@ -36,13 +37,19 @@ if exist %OUTPUT_DIR% rd /s /q %OUTPUT_DIR%
    %TARGET_PRJ%
 if not %errorlevel%==0 goto end
 
+xcopy /y %OUTPUT_DIR%\x86 %OUTPUT_DIR%\Lib\x86
+xcopy /y %OUTPUT_DIR%\x64 %OUTPUT_DIR%\Lib\x64
+rd /s /q %OUTPUT_DIR%\x86
+rd /s /q %OUTPUT_DIR%\x64
+rd /s /q %OUTPUT_DIR%\arm64
+
 rd /s /q %OUTPUT_DIR%\Lib\x86
 rd /s /q %OUTPUT_DIR%\libvlc\win-x86
 move %OUTPUT_ROOT%\FutaMaki\futamaki.dll.config %OUTPUT_ROOT%\FutaMaki\futamaki.exe.config
 
 @rem 公開用ZIPファイルの生成
 @rem 既に存在してる場合は設定がおかしいのであえて-Forceはつけない
-echo 圧縮してます…
+rem echo 圧縮してます…
 powershell -Command "Compress-Archive -Path %OUTPUT_DIR% -DestinationPath %OUTPUT_ROOT%\%OUTPUT_ZIP%"
 
 :end
