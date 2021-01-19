e93a726839b16cfa84e2c4b30bf07c7b9f145114
azumyar <yarukizero.dev@gmail.com>
2021-01-19 23:11:28 +0900

    スタイル修正

--- src/wpf/MakiMoki.Wpf/Converters/StyleConverter.cs
+++ src/wpf/MakiMoki.Wpf/Converters/StyleConverter.cs
@@ -11,18 +11,53 @@ using System.Windows.Media;
 namespace Yarukizero.Net.MakiMoki.Wpf.Converters {
 	class BackgroundToForegroundColorConverter : IMultiValueConverter {
 		public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) {
-			if(4 < values.Length) {
+			if(7 < values.Length) {
 				throw new ArgumentException("型不正。", nameof(values));
 			}
 
-			if((values[0] is SolidColorBrush back)
-				&& (values[1] is PlatformData.StyleType type)
-				&& (values[2] is Color white)
-				&& (values[3] is Color black)) {
+			//System.Diagnostics.Debug.WriteLine($"{ values.Length }");
+			{
+				/*
+				if(7 == values.Length) {
+					System.Diagnostics.Debug.WriteLine($"{ values[0] }");
+					System.Diagnostics.Debug.WriteLine($"{ values[1] }");
+					System.Diagnostics.Debug.WriteLine($"{ values[2] }");
+					System.Diagnostics.Debug.WriteLine($"{ values[3] }");
+					System.Diagnostics.Debug.WriteLine($"{ values[4] }");
+					System.Diagnostics.Debug.WriteLine($"{ values[5] }");
+					System.Diagnostics.Debug.WriteLine($"{ values[6] }");
+				}
+				*/
+				if((values[0] is Color back)
+					&& (values[1] is PlatformData.StyleType type)
+					&& (values[2] is Color white)
+					&& (values[3] is Color black)
+					&& (values[4] is Color orig)
+					&& (values[5] is FrameworkElement el)
+					&& (values[6] is VisualStateGroup g)) {
 
-				return (back.Color.A == 0) ? black : WpfUtil.ImageUtil.GetTextColor(back.Color, white, black, type);
+					var isAnimate = false;
+					try {
+						isAnimate = g.CurrentState?.Storyboard?.GetCurrentState(el) == System.Windows.Media.Animation.ClockState.Active;
+					}
+					catch(InvalidOperationException) {}
+					
+					var b = ((back.A == 0) || isAnimate) ? orig : back;
+					var foreground = (b.A == 0) ? black : WpfUtil.ImageUtil.GetTextColor(b, white, black, type);
+					//System.Diagnostics.Debug.WriteLine($"return:{ foreground }");
+					return foreground;
+				}
 			}
 
+			{
+				if((values[0] is SolidColorBrush back)
+					&& (values[1] is PlatformData.StyleType type)
+					&& (values[2] is Color white)
+					&& (values[3] is Color black)) {
+
+					return (back.Color.A == 0) ? black : WpfUtil.ImageUtil.GetTextColor(back.Color, white, black, type);
+				}
+			}
 			// values[0]が設定されていない場合があるので固定値を返す
 			return values[3];
 		}
