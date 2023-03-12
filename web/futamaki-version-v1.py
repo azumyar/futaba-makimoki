69cc25bbaef9b29ed7547a896c7094ee40110a8b
azumyar <yarukizero.dev@gmail.com>
2023-03-12 22:42:24 +0900

    バージョンpyを更新

--- web/futamaki-version-v1.py
+++ web/futamaki-version-v1.py
@@ -6,6 +6,10 @@ import json
 
 targetPath = "/virtual/yarukizero/public_html/futamaki/"
 baseUrl = "https://dev.yarukizero.net/futamaki/"
+releaseConf = "/virtual/yarukizero/futamaki/canary.version.json"
+releaseUrl = "https://install.appcenter.ms/users/azumyar/apps/futamaki/distribution_groups/release"
+canaryConf = "/virtual/yarukizero/futamaki/canary.version.json"
+canaryUrl = "https://install.appcenter.ms/users/azumyar/apps/futamaki/distribution_groups/canary"
 
 ret = {}
 files = glob.glob(targetPath + "*.zip")
@@ -20,6 +24,20 @@ if 0 < len(files):
         p = int(m.group(1))
     ret = { "period": p, "file": f, "url": (baseUrl + f) }
 
+f = open(releaseConf, "r")
+js =json.load(f)
+f.close()
+
+#ret["release-version"] = js["version"]
+#ret["release-url"] = releaseUrl
+
+f = open(canaryConf, "r")
+js =json.load(f)
+f.close()
+
+ret["canary-version"] = js["version"]
+ret["canary-url"] = canaryUrl
+
 print("Content-type: application/json; charset=UTF-8")
-print('')
+print("")
 print(json.dumps(ret))
