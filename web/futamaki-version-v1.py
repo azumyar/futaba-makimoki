6aa207f3292653bac799bbb186b42206bf1d5773
azumyar <yarukizero.dev@gmail.com>
2023-04-08 20:40:16 +0900

    サイトの更新

--- web/futamaki-version-v1.py
+++ web/futamaki-version-v1.py
@@ -4,38 +4,38 @@ import os
 import re
 import json
 
+# version.jsonのapp-center-versionは今の時点で入ってないので今後実装
+
 targetPath = "/virtual/yarukizero/public_html/futamaki/"
 baseUrl = "https://dev.yarukizero.net/futamaki/"
-releaseConf = "/virtual/yarukizero/futamaki/canary.version.json"
+releaseConf = "/virtual/yarukizero/futamaki/release.version.json"
 releaseUrl = "https://install.appcenter.ms/users/azumyar/apps/futamaki/distribution_groups/release"
 canaryConf = "/virtual/yarukizero/futamaki/canary.version.json"
 canaryUrl = "https://install.appcenter.ms/users/azumyar/apps/futamaki/distribution_groups/canary"
 
 ret = {}
-files = glob.glob(targetPath + "*.zip")
-if 0 < len(files):
-    files.sort(
-        key=lambda x: int(os.path.getctime(x)),
-        reverse=True)
-    f = os.path.split(files[0])[1]
-    m = re.match("^futamaki-0*(\\d+)\\.zip", f)
-    p = None
-    if m:
-        p = int(m.group(1))
-    ret = { "period": p, "file": f, "url": (baseUrl + f) }
-
 f = open(releaseConf, "r")
 js =json.load(f)
 f.close()
 
-#ret["release-version"] = js["version"]
-#ret["release-url"] = releaseUrl
+m = re.match("^\\d+\.(\\d+)\\..*$", js["version"])
+p = None
+if m:
+    p = int(m.group(1))
+
+ret["period"] = p
+ret["file"] = None
+ret["url"] = releaseUrl
+ret["release-version"] = js["version"]
+ret["release-app-center-version"] = js["app-center-version"]
+ret["release-url"] = releaseUrl
 
 f = open(canaryConf, "r")
 js =json.load(f)
 f.close()
 
 ret["canary-version"] = js["version"]
+ret["canary-app-center-version"] = js["app-center-version"]
 ret["canary-url"] = canaryUrl
 
 print("Content-type: application/json; charset=UTF-8")
